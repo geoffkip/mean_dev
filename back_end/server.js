06ea3c537a19485f0d10fb7ca52101098e6a5e9e
Geoffrey Kip <g_kip@live.com>
2017-11-21 22:35:33 -0500

    message testing app

--- back_end/server.js
+++ back_end/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app= express()
 var bodyParser= require('body-parser');
-var mongo= require('mongodb').MongoClient;
+var mongoose=  require('mongoose');
 
-var database;
+var Message = mongoose.model('Message',{
+  msg: String
+});
 
 app.use(bodyParser.json());
 app.use(function(req,res,next){
@@ -12,16 +14,26 @@ app.use(function(req,res,next){
   next()
 })
 
+app.get('/api/message',GetMessages);
+
 app.post('/api/message', function(req,res){
   console.log(req.body);
-  database.collection('messages').insertOne(req.body)
+  var message= new Message(req.body);
+  message.save()
+
   res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test",function(err,db){
+function GetMessages(req,res)
+{
+  Message.find({}).exec(function(err,result){
+    res.send(result);
+  })
+}
+
+mongoose.connect("mongodb://localhost:27017/test",function(err,db){
   if(!err){
     console.log("we are connected to mongo");
-    database=db;
   }
 })
 
--- front_end/src/app/index.module.js
+++ front_end/src/app/index.module.js
@@ -3,8 +3,8 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-import { MainController } from './main/main.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
+import { AuthController } from './auth/auth.controller';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
@@ -18,5 +18,6 @@ angular.module('myMessageFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSaniti
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
