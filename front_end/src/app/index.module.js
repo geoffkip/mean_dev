f4d36a8f76c34afb881850972b786d9244cc623a
Geoffrey Kip <g_kip@live.com>
2017-11-25 13:04:46 -0500

    starting authentication for message board app

--- front_end/src/app/index.module.js
+++ front_end/src/app/index.module.js
@@ -3,13 +3,15 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
+import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
+import { CompareToDirective } from './directives/compareTo.directive';
+import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
-angular.module('myMessageFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr'])
+angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
@@ -20,4 +22,5 @@ angular.module('myMessageFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSaniti
   .controller('MainController', MainController)
   .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
+  .directive('acmeMalarkey', MalarkeyDirective)
+  .directive('compareTo', CompareToDirective);
